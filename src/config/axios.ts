736d58ea025f9b3bf0b5dcadfb1b79c964789569
Raul Suarez <rsuarez@usa.pc-depot.com>
2022-08-23 16:25:08 -0400

    working on

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -3,11 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+//export const axios = axiosProvider.create({
+//  timeout: Number(process.env.AXIOS_TIMEOUT) || 5000,
+//  baseURL: process.env.DEPLOY_PLATFORM_URL || '',
+//  headers: {
+//    Authorization: `Bearer ${process.env.DEPLOY_PLATFORM_TOKEN}`,
+//  },
+//  params: {
+//    target: 'preview',
+//    teamId: process.env.DEPLOY_PLATFORM_TEAM_ID || '',
+//  },
+//});
+
 export const axios = axiosProvider.create({
   timeout: Number(process.env.AXIOS_TIMEOUT) || 5000,
   baseURL: process.env.DEPLOY_PLATFORM_URL || '',
   headers: {
-    Authorization: `Bearer ${process.env.DEPLOY_PLATFORM_TOKEN}`,
+    Authorization: `Bearer 12345`,
   },
   params: {
     target: 'preview',
--- src/config/discord.ts
+++ src/config/discord.ts
@@ -1,6 +1,12 @@
+//export default {
+//  webhookId: process.env.DISCORD_WEBHOOK_ID || '',
+//  webhookToken: process.env.DISCORD_WEBHOOK_TOKEN || '',
+//  webhookUsername: process.env.DISCORD_WEBHOOK_USERNAME || '',
+//  webhookAvatarURL: process.env.DISCORD_WEBHOOK_IMAGE_URL || '',
+//};
 export default {
-  webhookId: process.env.DISCORD_WEBHOOK_ID || '',
-  webhookToken: process.env.DISCORD_WEBHOOK_TOKEN || '',
-  webhookUsername: process.env.DISCORD_WEBHOOK_USERNAME || '',
-  webhookAvatarURL: process.env.DISCORD_WEBHOOK_IMAGE_URL || '',
+  webhookId: "1010972203195568238",
+  webhookToken: "O1HVwKZkudQbi0QyX9RgjpTtfbmANpkWvAwk4J3h291Rer51LfZ-keEX1URIyIjP8Lwn",
+  webhookUsername: "Spidey Bot",
+  webhookAvatarURL: '',
 };
